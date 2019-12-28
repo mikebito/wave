916399af62000be75b23ce8373d95ad3047f42f7
mikebito <30741054+mikebito@users.noreply.github.com>
2019-12-28 15:13:09 +0900

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,7 +1,6 @@
 import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt 
-import wave
 
 chunk = 1024
 FORMAT = pyaudio.paInt16
@@ -9,7 +8,6 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 3
-WAVE_OUTPUT_FILENAME = "output.wav"
 p = pyaudio.PyAudio()
 
 stream = p.open(format = FORMAT,
@@ -33,31 +31,14 @@ print("Finished Recording.")
 stream.close()
 p.terminate()
 
-
 data = b"".join(all)
 result = np.frombuffer(data,dtype="int16") / float (2**15)
-
-
-
-a = np.frombuffer(data,dtype="int16")
-result2 = np.frombuffer(data,dtype="int8") / float (2**15)
-result3 = np.frombuffer(data,dtype="int32") / float (2**15)
-result4 = np.frombuffer(data,dtype="int64") / float (2**15)
 minus = result * -1
 
 
 plt.plot(result, color = "blue", linestyle = "-")
 plt.plot(minus, color = "cyan", linestyle = "--")
-# plt.plot(result2, color = "red", linestyle = "--")
-# plt.plot(result3, color = "green", linestyle = "-.")
-# plt.plot(a, color = "magenta", linestyle = "-")
 plt.savefig('wave.png')
 plt.savefig('wave.pdf')
 plt.show()
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(all)) 
-wf.close()
